136e6dbb3abcc8045d06d806b501b3fc2574c6ab
rinatoz <57804004+rinatoz@users.noreply.github.com>
2019-12-21 16:54:41 +0200

    update main , without captions or explenantion

--- main1.c
+++ main1.c
@@ -6,12 +6,10 @@ int main()
 	int arr[SIZE];
 	for (int j = 0; j < SIZE; j++)
 	{
-		printf("enter %d num:", j);
+		
 		scanf("%d", &*(arr+j));
 	}
-	printf("\n");
 	insertion_sort(arr, SIZE);
-	printf("after the sortion\n\n");
 	for (int j = 0; j < SIZE; j++)
 	{
 		if (j < SIZE - 1)
